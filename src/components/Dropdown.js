141c086f5bef6a66c9461c5a5fb792385be439da
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-07-12 14:24:58 +0530

    React dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import styles from '../styles/dropdown.module.css';
+import ArrrowIcon from '../images/down-arrow.png';
+
+// Dropdown component
+function Dropdown() {
+  const items = ['...', 'Yes', 'Probably not']; //dropdown items list
+  const [dropdownValue, setDropdownValue] = useState('');  // state to maintain selected item of dropdown
+  const [displayDropdown, setDisplayDropdown] = useState(false); //state to show dropdown items
+
+  // Handle clicking an option from dropdown list
+  const handleSelect = (e) => {
+    const data = e.target.innerHTML;
+
+    // if selected option is not default
+    if(data !== '...'){
+      setDropdownValue(data);
+    }else{
+      setDropdownValue('');
+    }
+    setDisplayDropdown(false);
+  }
+
+  return (
+    <div className={styles.dropdownContainer}>
+      <p>Should you use a dropdown?</p>
+
+      {/* Dropdown switch */}
+      <button className={styles.btn} onMouseEnter={() => setDisplayDropdown(true)}>
+        <p>
+          {dropdownValue !== '' ? dropdownValue : 'Select'}
+        </p>
+       <img src={ArrrowIcon} alt='dropdown-icon' height='50px' width='50px' />
+      </button>
+
+      {/* Dropdown options */}
+      {displayDropdown && (
+        <div className={styles.dropdownList}>
+          <ul>
+            { items.map((item, index) => (
+                <li onClick={handleSelect} value={item} key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}
+
+// export the component
+export default Dropdown;
